3002ed3f17cba12dbf15896009e774b55f132393
Brett V. Forsgren <brett.forsgren@outlook.com>
2019-10-15 12:34:53 -0700

    allow building without tests

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,7 +1,9 @@
 @echo off
 setlocal
 
+set thisdir=%~dp0
 set configuration=Debug
+set runtests=true
 
 :parseargs
 if "%1" == "" goto argsdone
@@ -11,16 +13,26 @@ if /i "%1" == "-c" (
     shift
     goto parseargs
 )
+if /i "%1" == "-notest" (
+    set runtests=false
+    shift
+    goto parseargs
+)
 
 echo Unsupported argument: %1
 goto error
 
 :argsdone
 
+:: build
 set SOLUTION=%~dp0IxMilia.Lisp.sln
 dotnet restore %SOLUTION%
 if errorlevel 1 exit /b 1
 dotnet build %SOLUTION% -c %configuration%
 if errorlevel 1 exit /b 1
-dotnet test %SOLUTION% -c %configuration%
-if errorlevel 1 exit /b 1
+
+:: test
+if /i "%runtests%" == "true" (
+    dotnet test "%SOLUTION%" -c %configuration% --no-restore --no-build
+    if errorlevel 1 goto error
+)
--- build-and-test.sh
+++ build-and-test.sh
@@ -1,14 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/sh -e
 
-scriptroot="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
+_SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
 
 CONFIGURATION=Debug
+RUNTESTS=true
 
 while [ $# -gt 0 ]; do
   case "$1" in
     --configuration|-c)
       CONFIGURATION=$2
       shift
+      ;;
+    --notest)
+      RUNTESTS=false
       ;;
     *)
       echo "Invalid argument: $1"
@@ -18,7 +22,12 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-SOLUTION=$scriptroot/IxMilia.Lisp.sln
+# build
+SOLUTION=$_SCRIPT_DIR/IxMilia.Lisp.sln
 dotnet restore $SOLUTION
 dotnet build $SOLUTION -c $CONFIGURATION
-dotnet test $SOLUTION -c $CONFIGURATION
+
+# test
+if [ "$RUNTESTS" = "true" ]; then
+    dotnet test $SOLUTION -c $CONFIGURATION --no-restore --no-build
+fi
