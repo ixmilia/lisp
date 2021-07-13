ecbc3181ff94ce4cea8951a6664fd8cd3e9595cc
Brett V. Forsgren <brett.forsgren@outlook.com>
2021-07-13 13:33:43 -0600

    rename parameters

--- src/IxMilia.Lisp/init.lisp
+++ src/IxMilia.Lisp/init.lisp
@@ -11,34 +11,34 @@
     ; an atom is anything other than a cons cell
     (not (consp a)))
 
-(defun second (list)
-    (first (rest list)))
-(defmacro cadr (list)
-    (second list))
+(defun second (items)
+    (first (rest items)))
+(defmacro cadr (items)
+    (second items))
 
-(defun third (list)
-    (first (rest (rest list))))
-(defmacro caddr (list)
-    (third list))
+(defun third (items)
+    (first (rest (rest items))))
+(defmacro caddr (items)
+    (third items))
 
-(defun nthcdr (n list)
+(defun nthcdr (n items)
     (if (= n 0)
-        list
-        (nthcdr (- n 1) (cdr list))))
+        items
+        (nthcdr (- n 1) (cdr items))))
 
 (defun nth (n x)
     "Returns the Nth element of the list X, counting from 0"
     (car (nthcdr n x)))
 
-(defun last (list)
-    (cond ((eql nil (cdr list)) list)                 ; last of a proper list
-          ((listp (cdr list))   (last (cdr list)))    ; not the end of still a list
-          (t                    list)))               ; end of improper list
+(defun last (items)
+    (cond ((eql nil (cdr items))    items)                  ; last of a proper list
+          ((listp (cdr items))      (last (cdr items)))     ; not the end of still a list
+          (t                        items)))                ; end of improper list
 
-(defun member (item list)
-    (cond ((eql nil list)           ())                          ; not found
-          ((eql item (car list))    list)                        ; found it
-          (t                        (member item (cdr list)))))  ; check deeper
+(defun member (item items)
+    (cond ((eql nil items)          ())                             ; not found
+          ((eql item (car items))   items)                          ; found it
+          (t                        (member item (cdr items)))))    ; check deeper
 
 (defun subsetp (subset superset)
     (cond ((eql nil subset)                 t)                                  ; always true
@@ -53,18 +53,18 @@
     (cond ((eql key (cdr (car table)))  (car table))                ; found it
           ((consp (cdr table))          (rassoc key (cdr table))))) ; check deeper if more items remain
 
-(defun sublis (table list)
-    (let ((keypair (assoc (car list) table)))
-        (cond ((eql nil list)   ())                                             ; empty/end of list
-              (keypair          (cons (cdr keypair) (sublis table (cdr list)))) ; perform replacement on head
-              (t                (cons (car list) (sublis table (cdr list))))))) ; no key/value pair found; continue
+(defun sublis (table items)
+    (let ((keypair (assoc (car items) table)))
+        (cond ((eql nil items)  ())                                                 ; empty/end of list
+              (keypair          (cons (cdr keypair) (sublis table (cdr items))))    ; perform replacement on head
+              (t                (cons (car items) (sublis table (cdr items)))))))   ; no key/value pair found; continue
 
 (defun subst (r s items)
     (sublis (list (cons s r)) items))
 
-(defun mapcar (fn list)
-    (cond ((eql nil list)       ())                                                         ; end of list
-          (t                    (cons (funcall fn (car list)) (mapcar fn (cdr list))))))    ; perform replacement on head
+(defun mapcar (fn items)
+    (cond ((eql nil items)      ())                                                         ; end of list
+          (t                    (cons (funcall fn (car items)) (mapcar fn (cdr items))))))  ; perform replacement on head
 
 ; just to ensure the script was properly loaded
 t
