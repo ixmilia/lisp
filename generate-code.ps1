d8b9055f6df5fd63c324de82fe7c923246542f29
Brett V. Forsgren <brett.forsgren@outlook.com>
2023-02-17 10:48:22 -0700

    fix path separators

--- generate-code.ps1
+++ generate-code.ps1
@@ -4,8 +4,8 @@ Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
 try {
-    $generatorProject = "$PSScriptRoot\src\IxMilia.Lisp.Generator\IxMilia.Lisp.Generator.csproj"
-    $contractsFile = "$PSScriptRoot\src\lisp-monaco-editor\src\grammar\contracts.ts"
+    $generatorProject = "$PSScriptRoot/src/IxMilia.Lisp.Generator/IxMilia.Lisp.Generator.csproj"
+    $contractsFile = "$PSScriptRoot/src/lisp-monaco-editor/src/grammar/contracts.ts"
     dotnet run --project $generatorProject $contractsFile
 }
 catch {
